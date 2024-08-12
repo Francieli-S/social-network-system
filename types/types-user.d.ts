442219cb54f6c9e8c04d22429adafac4ed6f41f9
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-08-12 15:28:58 +0200

    add user and auth types

--- types/types-user.d.ts
+++ types/types-user.d.ts
@@ -2,6 +2,24 @@ interface IUser {
   id: number
   name: string
   profilePicture: string
-  friends: IUser[]
-  status: string
+  friends: number[]
+  statusMessage: string
+}
+
+interface IAuth {
+  isLoggedIn: boolean
+  currentUserId: number
+}
+
+interface UserState {
+  users: IUser[]
+}
+
+interface AuthState {
+  auth: IAuth
+}
+
+interface RootState {
+  userState: UserState
+  userAuth: AuthState
 }
