136df3514f5872d3587d83bcb23201be110b24d7
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-08-13 15:18:37 +0200

    add actions

--- src/store/actions/user-action.ts
+++ src/store/actions/user-action.ts
@@ -1,10 +1,24 @@
 // add user
+export function addUser(user: IUser) {
+  const action: UserAction = {
+    type: 'ADD_USER',
+    user,
+  };
+}
 
 // set user status message
-
 // update user status message
+export function setStatusMessage(user: IUser) {
+  const action: UserAction = {
+    type: 'SET_STATUS_MESSAGE',
+    user,
+  };
+}
 
 // add user's friends.id
-
-// add authenticated user
-
+export function addFriend(user: IUser) {
+  const action: UserAction = {
+    type: 'ADD_FRIEND',
+    user,
+  };
+}
