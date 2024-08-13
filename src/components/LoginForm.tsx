b2d94bd8e438502e6c4ce02c9031a918a5a7dfc9
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-08-13 15:23:50 +0200

    add LoginForm component and functions to access and update the AuthState - NOT WORKING YET

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,24 +1,34 @@
 'use client'
+import { addAuthUser } from '@/store/actions/auth-action';
+import { RootState } from '@/store/store';
 import * as React from 'react'
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { Dispatch } from 'redux';
 
 const LoginForm: React.FC = () => {
-  const [user, setUser] = React.useState<number | null>()
+  const [userId, setUserId] = React.useState('')
+  
+  const authState = useSelector((state: RootState) => state.reducerAuth.auth)
 
-  const handleUserData = (e: React.FormEvent<HTMLInputElement>) => {
-    setUser(e.currentTarget.value)
-  }
+  const dispatch: Dispatch<any> = useDispatch()
 
-  const authUser = (e: React.FormEvent) => {
+  const login = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
-
+    console.log('data before login', authState);
+    const action: AuthAction = {type: 'LOGIN', auth: { isLoggedIn: true, currentUserId: +userId} }
+    dispatch(addAuthUser(action.auth))
+    console.log('data after login', authState);
   }
+
+  // redirect
 
   return (
     <>
       <h1>Log in</h1>
-      <form onSubmit={authUser}>
-        <input type='text' id='id' placeholder='enter your id' value={user?.id} onChange={handleUserData} />
-        <button>Log in</button>
+      <form>
+        <input type='text' id='userId' placeholder='enter your id' value={userId} onChange={e => setUserId(e.target.value)} />
+        <button onClick={login}>Log in</button>
       </form>
     </>
   );
