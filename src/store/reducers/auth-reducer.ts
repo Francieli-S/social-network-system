31b7df5e48725f317ba6a26967610251564eca4a
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-08-14 15:54:11 +0200

    add StateUserAuth type to access users array to check if user is registered when calling LOGIN action

--- src/store/reducers/auth-reducer.ts
+++ src/store/reducers/auth-reducer.ts
@@ -1,22 +1,50 @@
-const initialAuthState: AuthState = {
-  auth: { isLoggedIn: false, currentUserId: null },
+type StateUsersAuth = {
+  users: IUser[];
+  auth: IAuth;
+};
+
+const initialState: StateUsersAuth = {
+  users: [
+    {
+      id: 1,
+      name: 'Foxy',
+      profilePicture: 'url',
+      friends: [2],
+      statusMessage: 'Online',
+    },
+    {
+      id: 2,
+      name: 'Gato',
+      profilePicture: 'url',
+      friends: [1],
+      statusMessage: 'Online',
+    },
+  ],
+  auth: { isLoggedIn: false, currentUserId: null, error: '' },
 };
 
 const reducerAuth = (
-  state: AuthState = initialAuthState,
+  state: StateUsersAuth = initialState,
   action: AuthAction
 ): AuthState => {
   switch (action.type) {
     case 'LOGIN':
-      const currentAuthUser: IAuth = {
-        isLoggedIn: true,
-        currentUserId: action.auth.currentUserId,
-      };
-      return { ...state, auth: currentAuthUser };
-
+      const registeredUser = state.users.some(
+        (user) => user.id === action.auth.currentUserId
+      );
+      if (registeredUser) {
+        return { ...state, auth: action.auth };
+      } else {
+        return {
+          ...state,
+          auth: { ...state.auth, error: 'User not registered' },
+        };
+      }
+    
+    // case: 'LOGOUT'
     default:
       return state;
   }
 };
 
-export default reducerAuth
+export default reducerAuth;
