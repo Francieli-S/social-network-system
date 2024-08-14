e4ad8821598813015b25d992f74e668efb0549f5
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-08-14 16:31:19 +0200

    refactor functionality to redirect not registered user to register page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,12 +5,11 @@ import { redirect } from 'next/navigation';
 import { useSelector, useDispatch } from 'react-redux';
 import { Dispatch } from 'redux';
 import { useEffect, useState } from 'react';
-import RedirectToRegister from '../../../components/RedirectToRegistet';
 
 const Login: React.FC = () => {
   const [userId, setUserId] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  const [userRegistered, setUserRegistered] = useState(true); // part the functionality to make redirection to /register
+  const [userRegistered, setUserRegistered] = useState(true);
 
   // it selects one of the two reducer: reducerAuth or reducerUser from the store
   const authState = useSelector((state: RootState) => state.reducerAuth.auth);
@@ -34,8 +33,13 @@ const Login: React.FC = () => {
 
   const handleRegisterRedirect = () => {
     setUserRegistered(false);
-    // redirect('/register'); DOES NOT WORK WITHOUT <RedirectToRegister /> component
   };
+  
+  useEffect(() => {
+    if (!userRegistered) {
+      redirect('/register'); 
+    }
+  }, [userRegistered])
 
   return (
     <>
@@ -54,7 +58,6 @@ const Login: React.FC = () => {
         <div>
           <p>{errorMessage}</p>
           <button onClick={handleRegisterRedirect}>Register</button>
-          {!userRegistered && <RedirectToRegister />}
         </div>
       ) : (
         ''
