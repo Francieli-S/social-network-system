bc7d0815ff7cd9004b3590c8924bc76e4e7e44ce
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-08-15 17:49:29 +0200

    remove unnecessary comments

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -6,11 +6,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import reducer, { StateUsersAuth } from './reducers/reducer';
 
-// const rootReducer = combineReducers({ reducerUser, reducerAuth });
-// from chatGPT - check later how it works
-// export type RootState = ReturnType<typeof rootReducer>;
-// const store: Store<RootState, UserAction | AuthAction> = configureStore({ rootReducer });
-
 const store: Store<StateUsersAuth> = configureStore({ reducer });
 
 const StoreWrapper = ({children}: React.PropsWithChildren) => {
