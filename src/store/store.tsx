4a91a98d5d1e7300b889ed2d5fce2b28d7a8718f
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-08-15 23:00:04 +0200

    remove Store type to let TS automatically inferrt when using configureStore <it had caused failure to deploy>

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -6,7 +6,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import reducer, { StateUsersAuth } from './reducers/reducer';
 
-const store: Store<StateUsersAuth> = configureStore({ reducer });
+const store = configureStore({ reducer });
 
 const StoreWrapper = ({children}: React.PropsWithChildren) => {
   return <Provider store={store}>{children}</Provider>
